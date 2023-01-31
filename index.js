3795b35f64984c95333247d1fe4aefedd0841edb
vineetcoding <114997519+vineetcoding@users.noreply.github.com>
2023-02-01 03:47:59 +0530

    js prectice1

--- index.js
+++ index.js
@@ -7,12 +7,13 @@
 //  const:-
 // 1. const can not re declaired
 
-
+console.log(b);
+// console.log(as);
+// let as;
 let a = 12;
 var b = 10;
 const c = 13;
 a = 20;
-// console.log(a);
 function xyz(){
     console.log(y);
 {
@@ -42,6 +43,7 @@ var shyam;
 
 const sita = 66;
 
+//==============================================
 // get time, date, secons, etc Day and months in the form of 0,1,2,3,4,...
 let presentTime =new Date().getDay();
 console.log(presentTime);
@@ -60,7 +62,7 @@ switch (new Date().getDay()) {
     text = "Looking forward to the Weekend";
 }
 console.log(text);
-
+//================================================
 // set is always store unique value only
 let arr =[2,4,5,2,9,6,1,5,11];
 let narr =new Set(arr);
@@ -69,3 +71,125 @@ narr.delete(2)
 console.log(narr.has(11));
 console.log(narr.size);
 console.log(narr);
+//=====================================================
+//object, map, entries
+let obj = {
+    name : "vineet",
+    nobj : {
+        class : 10,
+        board : "state",
+        result : "pass"
+    },
+    age : 25,
+    color : "fair",
+    skill : "coding"
+}
+console.log(obj.nobj.board);
+//======================================================
+//double all element inside the array using map Method
+    let ar = [4,2,8,5,1];
+    console.log(ar.map((e)=>{
+        return e*2
+    }));
+
+//find even number using filter method
+    console.log(ar.filter((e)=>{
+        if(e%2 == 0){
+            return e;
+        }
+    }))
+
+// find max using reduce method
+    console.log(ar.reduce((max,curr)=>{
+        if(curr > max){
+            max = curr;
+        }
+        return max;
+    }))
+//========================================================
+// Bitwise Operators and, or
+let BitwiseAnd = 5 & 1;   //5 = 0 1 0 1 ,  1 = 0 0 0 1 => output 0 0 0 1 = 1
+console.log(BitwiseAnd);
+
+let BitwiseOr = 5 || 1;    // 5 = 0 1 0 1 ,  1 = 0 0 0 1 => output 0 1 0 1 = 5
+console.log(BitwiseOr);
+//=======================================================
+// Error Try an catch
+try {
+    console.log("Block of code to try");
+  }
+  catch(err) {
+    console.log("Block of code to handle errors")
+  }
+//======================================================
+  function strict() {
+    // "use strict"
+    // return "use of strict"
+    a = 50;
+    return a;
+  }
+  console.log(strict());
+
+//   "use strict"
+//   c = 100;
+//   console.log(c);
+//========================================================
+//  this Keyword  => this keyword refers to current object
+const person = {
+    firstName: "John",
+    lastName : "Doe",
+    id       : 5566,
+    fullName : function() {
+      return this.firstName + " " + this.lastName;
+    }
+  };
+console.log(person.fullName());
+
+// call methot in this => it call this keword object value in other object
+const person1 = {
+    fullName: function() {
+      return this.firstName + " " + this.lastName;
+    }
+  }
+
+  const person2 = {
+    firstName:"John",
+    lastName: "Doe",
+  }
+  console.log(person1.fullName.call(person2));  // Return "John Doe"
+
+// bind method in object => it can borrow a method from other object(it borrow as a function)
+const persons = {
+    firstName:"John",
+    lastName: "Doe",
+    fullName: function () {
+      return this.firstName + " " + this.lastName;
+    }
+  }
+  
+  const member = {
+    firstName:"Hege",
+    lastName: "Nilsen",
+  }
+//   console.log(person.fullName.bind(member)());
+  let fullName = persons.fullName.bind(member);
+  console.log(fullName());
+
+// apply method =>it work like call only diffrence is apply() method accepts arguments in an array
+const personss = {
+    fullName: function(city, country) {
+      return this.firstName + " " + this.lastName + "," + city + "," + country;
+    }
+  }
+  
+  const person1s = {
+    firstName:"John",
+    lastName: "Doe"
+  }
+  
+console.log(personss.fullName.apply(person1, ["Oslo", "Norway"]));
+//==================================================================================
+//sort array
+let arrs = [6,41,8,3,9,2];
+arrs.sort((a,b)=>a-b);
+console.log(arrs);
