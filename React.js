f8426000f0250dbf7ac18e0d249150d7cb9637ba
vineetcoding <114997519+vineetcoding@users.noreply.github.com>
2023-02-17 02:57:45 +0530

    React

--- React.js
+++ React.js
@@ -1,3 +1,60 @@
+// Q.1=> what is react js ?
+// Ans=> js stand for javascript it's a advance library of js,
+//       it's use to create very fast UI(frontend) designe,
+//       it's open source library and Developed and managed by Faceook
+//       it did not render whole page again and again, it render only that component which we want to use
+
+// Q.2=> Features of React ?
+// Ans=> 1- JSX => js + HTML
+//       2- Components
+//       3- One-way data binding(source to destination) => parent to child (there is a different way of transfer data child to parent)
+//       4- Virtual DOM => 
+//       5- Simplicity
+//       6- Rective Performance
+
+// Q.3=> Unidirectional flow in React ?
+// Ans=> 
+
+
+// 4. What is Virtual DOM ?
+// 5. Explain how Virtual DOM works ?
+// 6. Difference between Real DOM and Virtual DOM ?
+// 7. What is JSX ?
+// 8. How does browser understand JSX ?
+// 9. What is babel ?
+// 10. Explain useState hook ?
+// 11. What are statefull and stateless components ?
+// 12. What is a key in React ?
+// 13. Why is it necessary ?
+// 14. In what cases we should not use array index as key ?
+// 15. Why should we not use array index as key ?
+// 16. What are synthetic events ?
+// 17. What are controlled components ?
+// 18. What are uncontrolled components ?
+// 19. What is the difference between the two ?
+// 20. What are props in react ?
+// 21. What is the difference between props and state in react ?
+// 22. What are the differenct lifecycle methods ?
+// 23. What are side effects in react ? 
+// 24. What is the useEffect hook ?
+// 25. How can we use useEffect hook to implement the lifecycle methods ?
+// 26. What is useRef hook ?
+// 27. Explain some use cases for useRef hook ?
+// 28. What are Higher Order Components ?
+// 29. What is a Pure component ?
+// 30. What is prop drilling ?
+// 31. How can we solve the problem of prop drilling ?
+// 31. What is Context API ? How it can be used to solve prop drilling ?
+// 32. What are error boundaries ?
+// 33. useCallback hook 
+// 34. useMemo hook
+// 35. Difference between the two
+// 36. What is React.memo ?
+// 37. State management 
+
+
+
+
 // what is jsx ==> html elements inside the component called jsx.
 
 //  <React.Fragment></React.Fragment> or <></>
